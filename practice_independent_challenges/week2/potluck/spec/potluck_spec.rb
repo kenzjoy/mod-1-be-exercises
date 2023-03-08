b7db98fd02b41f8391e01c6c7e58f19dc7121bc4
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-08 15:08:48 -0700

    creates test for get_all_from_category method

--- practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
+++ practice_independent_challenges/week2/potluck/spec/potluck_spec.rb
@@ -6,7 +6,12 @@ RSpec.describe Potluck do
     @potluck = Potluck.new('7-13-18')
     @couscous_salad = Dish.new('Couscous Salad', :appetizer)
     @cocktail_meatballs = Dish.new('Cocktail Meatballs', :entre)
+    @summer_pizza = Dish.new('Summer Pizza', :appetizer)
+    @roast_pork = Dish.new('Roast Pork', :entre)
+    @candy_salad = Dish.new('Candy Salad', :dessert)
   end
+
+  # Iteration 2
 
   it 'exists' do
     expect(@potluck).to be_a(Potluck)
@@ -24,6 +29,20 @@ RSpec.describe Potluck do
 
       expect(@potluck.dishes).to eq([@couscous_salad, @cocktail_meatballs])
       expect(@potluck.dishes.length).to eq(2)
+    end
+  end
+
+  # Iteration 3
+
+  describe '#get_all_from_category(category)' do
+    it 'returns all of the dished from a specified category' do
+      @potluck.add_dish(@couscous_salad)
+      @potluck.add_dish(@summer_pizza)
+      @potluck.add_dish(@roast_pork)
+      @potluck.add_dish(@cocktail_meatballs)
+      @potluck.add_dish(@candy_salad)
+
+      expect(@potluck.get_all_from_category(:appetizer).to eq(@couscous_salad, @summer_pizza))
     end
   end
 end
