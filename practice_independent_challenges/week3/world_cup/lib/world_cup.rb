0d1d4ab7c68bb6c054297ffbe37a1751c94d5bdc
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-09 12:59:16 -0700

    adds active player array to method

--- practice_independent_challenges/week3/world_cup/lib/world_cup.rb
+++ practice_independent_challenges/week3/world_cup/lib/world_cup.rb
@@ -1,9 +1,21 @@
 class WorldCup
   attr_reader :year, 
               :teams
-              
+
   def initialize(year, teams)
     @year = year
     @teams = teams
+  end
+
+  def active_players_by_position(position)
+    active_players = []
+    @teams.each do |team|
+      team.players.each do |player|
+        if player.position == position
+          active_players << player
+        end
+      end
+    end
+    active_players
   end
 end
