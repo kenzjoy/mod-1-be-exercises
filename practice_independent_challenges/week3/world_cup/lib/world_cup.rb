68d291f3f99c5644ee1463eaa0d1d26726d112d4
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-21 11:43:44 -0600

    picking back up where I left off

--- practice_independent_challenges/week3/world_cup/lib/world_cup.rb
+++ practice_independent_challenges/week3/world_cup/lib/world_cup.rb
@@ -20,4 +20,32 @@ class WorldCup
     end
     active_players
   end
+  
+  def all_players_by_position
+    sorted = @teams.each do |team|
+      team.players.sort_by do |player|
+        player.position
+      end
+    end
+
+    players_hash = {
+      'forward' => [],
+      'midfielder' => [],
+      'defender' => []
+    }
+
+    sorted.each do |player|
+      require 'pry'; binding.pry
+      player.each do |p|
+        if p.position == 'forward'
+          players_hash[:forward] << player
+        elsif p.position == 'midfielder'
+          players_hash[:midfielder] << player
+        elsif p.position == 'defender'
+          players_hash[:defender] << player
+        end
+      end
+    end
+    players_hash
+  end
 end
