2ec864e4b6835d006eba2338aeb8a8cdcbdd1a9e
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-21 18:30:24 -0600

    dragon can eat a lot

--- ruby_exercises/mythical-creatures/lib/dragon.rb
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -8,10 +8,18 @@ class Dragon
     @name = name
     @color = color
     @rider = rider
-    @hungry = true 
+    @hungry = true
+    @eat_counter = 0
   end
 
   def hungry?
     @hungry
+  end
+  
+  def eat
+    @eat_counter += 1
+    if @eat_counter >= 3
+      @hungry = false
+    end
   end
 end
