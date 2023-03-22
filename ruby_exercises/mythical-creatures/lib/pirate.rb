05a263353ff4430ba2a0803e16169122421115d7
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-22 13:03:33 -0600

    pirate has no booty

--- ruby_exercises/mythical-creatures/lib/pirate.rb
+++ ruby_exercises/mythical-creatures/lib/pirate.rb
@@ -1,12 +1,14 @@
 class Pirate 
   attr_reader :name,
-              :job
+              :job,
+              :booty
 
   def initialize(name, job = 'Scallywag')
     @name = name
     @job = job
     @cursed = false
     @heinous_acts = 0
+    @booty = 0
   end
 
   def commit_heinous_act
