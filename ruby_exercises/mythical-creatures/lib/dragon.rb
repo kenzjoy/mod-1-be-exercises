98de594f8ae0c5bbc351eb8bcc611f4004d8d991
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-21 18:22:35 -0600

    instantiates a new dragon

---
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -0,0 +1,11 @@
+class Dragon
+  attr_reader :name, 
+              :color,
+              :rider
+
+  def initialize(name, color, rider)
+    @name = name
+    @color = :color
+    @rider = rider
+  end
+end
