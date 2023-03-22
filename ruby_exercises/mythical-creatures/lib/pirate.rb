71be44103ee490f97f9b0755f611f4e841fa64cf
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-22 12:58:27 -0600

    pirate is or is not cursed

--- ruby_exercises/mythical-creatures/lib/pirate.rb
+++ ruby_exercises/mythical-creatures/lib/pirate.rb
@@ -5,5 +5,15 @@ class Pirate
   def initialize(name, job = 'Scallywag')
     @name = name
     @job = job
+    @cursed = false
+    @heinous_acts = 0
+  end
+
+  def commit_heinous_act
+    @heinous_acts += 1
+  end
+
+  def cursed?
+    @heinous_acts > 2
   end
 end
