438bf4f4cd5944ca16c222f33eab6cf5746a4f84
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-21 18:19:02 -0600

    thirsty by default

--- ruby_exercises/mythical-creatures/spec/vampire_spec.rb
+++ ruby_exercises/mythical-creatures/spec/vampire_spec.rb
@@ -17,12 +17,12 @@ RSpec.describe Vampire do
     expect(vampire.pet).to eq('bat')
   end
 
-  xit 'can keep other pets' do
+  it 'can keep other pets' do
     vampire = Vampire.new('Varney', 'fox')
     expect(vampire.pet).to eq('fox')
   end
 
-  xit 'is thirsty by default' do
+  it 'is thirsty by default' do
     vampire = Vampire.new('The Count')
 
     expect(vampire.thirsty).to be true
