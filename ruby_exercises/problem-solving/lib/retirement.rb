98fbc21ea4e767179fb081cb3da3c4524b830c4f
Mackenzie Leng <kenz.joy@outlook.com>
2023-03-30 14:41:51 -0600

    errors with a negative age

--- ruby_exercises/problem-solving/lib/retirement.rb
+++ ruby_exercises/problem-solving/lib/retirement.rb
@@ -1,5 +1,9 @@
 class Retirement
   def calculate(age_1, age_2)
-    "You have #{age_2 - age_1} years left until you can retire. It is 2015, so you can retire in #{2015 + (age_2 - age_1)}."
+    if age_1.negative? || age_2.negative?
+      "Error. Age cannot be negative."
+    else
+      "You have #{age_2 - age_1} years left until you can retire. It is 2015, so you can retire in #{2015 + (age_2 - age_1)}."
+    end
   end
 end
